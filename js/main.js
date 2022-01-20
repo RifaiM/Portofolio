2fc70956c85e764fbcac2ad4a96abdf70c372480
DPR - <rifaialc@gmail.com>
2022-01-20 13:11:08 +0700

    update

--- js/main.js
+++ js/main.js
@@ -55,8 +55,8 @@ $( '.js-input' ).keyup(function() {
   }
 });
 
+
 //scroll reveal
-//ScrollReveal().reveal('.testing');
 
 document.addEventListener("DOMContentLoaded", function(event) {
   document.addEventListener("scroll", function(event) {
@@ -110,30 +110,3 @@ function addClass(element, className) {
 //
 
 
-function resizeNav() {
-  // Set the nav height to fill the window
-  $("#nav-fullscreen").css({"height": window.innerHeight});
-
-  // Set the circle radius to the length of the window diagonal,
-  // this way we're only making the circle as big as it needs to be.
-  var radius = Math.sqrt(Math.pow(window.innerHeight, 2) + Math.pow(window.innerWidth, 2));
-  var diameter = radius * 2;
-  $("#nav-overlay").width(diameter);
-  $("#nav-overlay").height(diameter);
-  $("#nav-overlay").css({"margin-top": -radius, "margin-left": -radius});
-}
-
-// Set up click and window resize callbacks, then init the nav.
-$(document).ready(function() {
-  $("#nav-toggle").click(function() {
-      $("#nav-toggle, #nav-overlay, #nav-fullscreen").toggleClass("open");
-  });
-
-  $(window).resize(resizeNav);
-
-  resizeNav();
-
-window.setTimeout(function() {
-   $("#nav-toggle").click();
-}, 1000)
-});
